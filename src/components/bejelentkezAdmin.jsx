4d1788d48378ed90ddc77b453f843a6f1562491d
Almaelado <borsny75@gmail.com>
2025-11-11 12:17:56 +0100

    mod

---
+++ src/components/bejelentkezAdmin.jsx
@@ -0,0 +1,25 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+function BasicExample() {
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Felhasználónév</Form.Label>
+        <Form.Control type="text" placeholder="Felhasználónév" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Jelszó</Form.Label>
+        <Form.Control type="password" placeholder="Jelszó" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Emlékezz rám" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Bejelentkezés
+      </Button>
+    </Form>
+  );
+}
+
+export default BasicExample;
