e5e67b200a2f7fd055f1532efc65e582abfed6a6
Almaelado <borsny75@gmail.com>
2025-11-20 09:18:35 +0100

    v0.2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import './App.css'; 
 import Autok from './components/autok.jsx';
-import Bejelentkez from './components/bejelentkezAdmin.jsx'
+import Bejelentkez from './components/bejelentkezes.jsx'
 import Szures from './components/szures.jsx'
+import Menu from './components/menu.jsx';
+
 import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [szur,setSzur] = useState("");
+  const [szur, setSzur] = useState("");
+
   return (
-      <>
-      <div className="d-flex justify-content-center">
+    <BrowserRouter>
+      <Menu />
+      <div className="App">
         <Szures onSearch={filterString => setSzur(filterString)} />
-      <Autok szuro={szur} />
-        </div>    
-      
-      </>
-  )
+        <Autok szuro={szur} />
+
+        {/* Példa route-ok */}
+        <Routes>
+          <Route path="/bejelentkez" element={<Bejelentkez />} />
+          {/* Itt jöhetnek a többi route-ok */}
+        </Routes>
+      </div>    
+    </BrowserRouter>
+  );
 }
 
 export default App;
